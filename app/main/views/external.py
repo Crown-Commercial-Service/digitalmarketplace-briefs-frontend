ae0fc3e2724fb2675f71c50ec063669f47b291d7
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-06-16 15:36:05 +0100

    Add external links to buyer app for user creation
    The new views for creating buyer accounts need to link back to the buyer
    app for actually creating users. The necessary links are added here in
    the same way as other external links

--- app/main/views/external.py
+++ app/main/views/external.py
@@ -4,3 +4,18 @@ from .. import external
 @external.route('/<framework_framework>/opportunities/<brief_id>')
 def get_brief_by_id(framework_framework, brief_id):
     raise NotImplementedError()
+
+
+@external.route('/create-user/<string:encoded_token>')
+def create_user(encoded_token):
+    raise NotImplementedError()
+
+
+@external.route('/create-your-account-complete')
+def create_your_account_complete():
+    raise NotImplementedError()
+
+
+@external.route('/login')
+def render_login():
+    raise NotImplementedError()
