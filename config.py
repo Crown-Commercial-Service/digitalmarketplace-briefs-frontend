6b89a7d55ad3e12ca76e8082b7d9219dd8b8eacf
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2017-09-06 14:20:41 +0100

    enables direct-award projects on preview

--- config.py
+++ config.py
@@ -131,7 +131,7 @@ class Live(Config):
 
 class Preview(Live):
     FEATURE_FLAGS_NEW_SUPPLIER_FLOW = enabled_since('2017-02-06')
-
+    FEATURE_FLAGS_DIRECT_AWARD_PROJECTS = enabled_since('2017-08-15')
 
 class Staging(Live):
     FEATURE_FLAGS_NEW_SUPPLIER_FLOW = enabled_since('2017-02-07')
