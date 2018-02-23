3fc56318a51704be8757b518edd547596ca6637f
George Lund <george.lund@digital.cabinet-office.gov.uk>
2018-02-23 15:48:16 +0000

    Login views for tests have moved into new test-utils repo.
    https://trello.com/c/lQIP4rf8/300-flash-messages-briefs-app

--- tests/helpers.py
+++ tests/helpers.py
@@ -13,43 +13,7 @@ from mock import patch
 from werkzeug.http import parse_cookie
 
 from dmutils.formats import DATETIME_FORMAT
-
-from flask import Blueprint
-from dmutils.user import User
-from flask_login import login_user
-
-
-login_for_tests = Blueprint('login_for_tests', __name__)
-
-
-@login_for_tests.route('/auto-buyer-login')
-def auto_buyer_login():
-    user_json = {"users": {
-        'id': 123,
-        'name': u'Ā Buyer',
-        'emailAddress': 'buyer@email.com',
-        'role': 'buyer'
-    }
-    }
-    user = User.from_json(user_json)
-    login_user(user)
-    return "OK"
-
-
-@login_for_tests.route('/auto-supplier-login')
-def auto_supplier_login():
-    user_json = {"users": {
-        'id': 123,
-        'name': u'Ā Buyer',
-        'emailAddress': 'supplier@email.com',
-        'supplierId': 1234,
-        'supplierName': 'Supplier Name',
-        'role': 'supplier',
-    }
-    }
-    user = User.from_json(user_json)
-    login_user(user)
-    return "OK"
+from dmtestutils.login import login_for_tests
 
 
 class BaseApplicationTest(object):
