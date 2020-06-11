c025537315fc5440e984c2c77b1811d6401f4cc1
BRETT KYLE <brett.kyle@digital.cabinet-office.gov.uk>
2020-06-11 16:19:21 +0100

    Add open_links_in_new_tab property

--- app/main/views/buyers.py
+++ app/main/views/buyers.py
@@ -491,7 +491,12 @@ def preview_brief_source(framework_slug, lot_slug, brief_id):
     # Get attributes in format suitable for govukSummaryList
     brief_summary = display_content.summary(brief)
     for section in brief_summary:
-        section.summary_list = to_summary_list_rows(section.questions, format_links=True, filter_empty=False)
+        section.summary_list = to_summary_list_rows(
+            section.questions,
+            format_links=True,
+            filter_empty=False,
+            open_links_in_new_tab=True
+        )
 
     # TODO: move preview_brief_source templates/includes into shared FE toolkit pattern to ensure it's kept in sync
     html = render_template(
