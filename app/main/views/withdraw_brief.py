128f74f7621579883988364aac73777ff81046c5
BRETT KYLE <brett.kyle@digital.cabinet-office.gov.uk>
2020-08-11 08:47:00 +0100

    Withdraw brief warning page added

--- app/main/views/withdraw_brief.py
+++ app/main/views/withdraw_brief.py
@@ -1,6 +1,8 @@
 from flask import abort, flash, redirect, url_for
 from flask_login import current_user
 
+from dmutils.flask import timed_render_template as render_template
+
 from app import data_api_client
 from .. import main
 from ..helpers.buyers_helpers import (
@@ -8,8 +10,28 @@ from ..helpers.buyers_helpers import (
     is_brief_correct,
 )
 
-
 BRIEF_WITHDRAWN_MESSAGE = "You’ve withdrawn your requirements for ‘{brief[title]}’"
+
+
+@main.route('/frameworks/<framework_slug>/requirements/<lot_slug>/<brief_id>/withdraw', methods=['GET'])
+def withdraw_a_brief_warning(framework_slug, lot_slug, brief_id):
+    framework, lot = get_framework_and_lot(
+        framework_slug,
+        lot_slug,
+        data_api_client,
+        allowed_statuses=['live', 'expired'],
+        must_allow_brief=True
+    )
+    brief = data_api_client.get_brief(brief_id)["briefs"]
+
+    if not is_brief_correct(brief, framework_slug, lot_slug, current_user.id, allowed_statuses=['live']):
+        abort(404)
+
+    return render_template(
+        "buyers/withdraw_brief.html",
+        framework=framework,
+        brief=brief,
+    ), 200
 
 
 @main.route('/frameworks/<framework_slug>/requirements/<lot_slug>/<brief_id>/withdraw', methods=['POST'])
@@ -28,5 +50,4 @@ def withdraw_a_brief(framework_slug, lot_slug, brief_id):
 
     data_api_client.withdraw_brief(brief_id, current_user.email_address)
     flash(BRIEF_WITHDRAWN_MESSAGE.format(brief=brief), "success")
-
     return redirect(url_for(".buyer_dos_requirements"))
