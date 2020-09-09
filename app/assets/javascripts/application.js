3a39a41280506562a135122aea5b9e58ade62c8a
BRETT KYLE <brett.kyle@digital.cabinet-office.gov.uk>
2020-09-09 08:46:00 +0100

    Remove jQuery
    Since the only production JavaScript is govuk-frontend and dm-govuk-frontend,
    it looks like we can remove jQuery as a dependency.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,7 +4,6 @@
   Sprockets-style (https://github.com/sstephenson/sprockets)
   directives to concatenate multiple Javascript files into one.
 */
-//= require ../../../node_modules/jquery/dist/jquery.js
 //= require ../../../node_modules/govuk-frontend/govuk/all.js
 //= require ../../../node_modules/digitalmarketplace-govuk-frontend/digitalmarketplace/all.js
 
